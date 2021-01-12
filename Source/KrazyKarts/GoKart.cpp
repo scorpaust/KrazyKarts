185515739a75d73d96b4c739e6c0dd4fa541e8b6
Dinis Costa <scorpaust@hotmail.com>
2021-01-12 19:56:44 +0000

    Applied movement and physics to the car.

--- Source/KrazyKarts/GoKart.cpp
+++ Source/KrazyKarts/GoKart.cpp
@@ -3,14 +3,28 @@
 
 #include "GoKart.h"
 #include "Components/InputComponent.h"
+#include "Components/BoxComponent.h"
 
+#pragma region Constructor Implementation
 // Sets default values
 AGoKart::AGoKart()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Collider = CreateDefaultSubobject<UBoxComponent>("Collider");
+
+	RootComponent = Collider;
+
+	MaxDegreesPerSecond = 90;
+
+	DragCoefficient = 16;
+
 }
+
+#pragma endregion
+
+#pragma region Begin  Play Implementation
 
 // Called when the game starts or when spawned
 void AGoKart::BeginPlay()
@@ -19,22 +33,32 @@ void AGoKart::BeginPlay()
 	
 }
 
+#pragma endregion
+
+#pragma region Tick Implementation
+
 // Called every frame
 void AGoKart::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	FVector Force;
+	FVector Force = GetActorForwardVector() * MaxDrivingForce * Throttle;
+
+	Force += GetResistance();
 
 	FVector Acceleration = Force / Mass;
 
 	Velocity = Velocity + Acceleration * DeltaTime;
 
-	FVector Translation = Velocity * 100 * DeltaTime;
+	ApplyRotation(DeltaTime);
 
-	AddActorWorldOffset(Translation);
+	UpdateLocationFromVelocity(DeltaTime);
 
 }
+
+#pragma endregion
+
+#pragma region Input Implementation
 
 // Called to bind functionality to input
 void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
@@ -42,14 +66,59 @@ void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AGoKart::MoveRight);
 
 }
+
+#pragma endregion
 
 #pragma region Movement
 
 void AGoKart::MoveForward(float Value) {
 
 	Throttle = Value;
+
+}
+
+void AGoKart::MoveRight(float Value) {
+
+	SteeringThrow = Value;
+
+}
+
+void AGoKart::ApplyRotation(float DeltaTime)
+{
+	float RotationAngle = MaxDegreesPerSecond * DeltaTime * SteeringThrow;
+
+	FQuat RotationDelta(GetActorUpVector(), FMath::DegreesToRadians(RotationAngle));
+
+	Velocity = RotationDelta.RotateVector(Velocity);
+
+	AddActorWorldRotation(RotationDelta);
+}
+
+#pragma endregion
+
+#pragma region Physics
+
+void AGoKart::UpdateLocationFromVelocity(float DeltaTime)
+{
+	FVector Translation = Velocity * 100 * DeltaTime;
+
+	FHitResult Hit;
+
+	AddActorWorldOffset(Translation, true, &Hit);
+
+	if (Hit.IsValidBlockingHit()) {
+
+		Velocity = FVector::ZeroVector;
+
+	}
+}
+
+FVector AGoKart::GetResistance() {
+
+	return -Velocity.GetSafeNormal() * Velocity.SizeSquared() * DragCoefficient;
 
 }
 
--- Source/KrazyKarts/GoKart.h
+++ Source/KrazyKarts/GoKart.h
@@ -12,31 +12,64 @@ class KRAZYKARTS_API AGoKart : public APawn
 	GENERATED_BODY()
 
 public:
+
+#pragma region Constructor
+	
 	// Sets default values for this pawn's properties
 	AGoKart();
 
+#pragma endregion
+
 protected:
+
+#pragma region Begin Play Function
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+#pragma endregion
+	
+
 public:	
+
+#pragma region Tick Function
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	void ApplyRotation(float DeltaTime);
+
+#pragma endregion
+	
+#pragma region Input Functions
+
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+#pragma endregion
 
 private:
 
 #pragma region Physics Variables
 
-	//Mass of the car (Kg)
+	// Mass of the car (Kg)
 	UPROPERTY(EditAnywhere, Category = "Physics")
 	float Mass = 1000;
 
-	//The force applied to the car when the throttle is fully down (N)
+	// The force applied to the car when the throttle is fully down (N)
 	UPROPERTY(EditAnywhere, Category = "Physics")
 	float MaxDrivingForce = 10000;
+
+	// The number of degrees rotated per second at full control throw (degree/s)
+	UPROPERTY(EditAnywhere, Category = "Physics")
+	float MaxDegreesPerSecond;
+
+	// Higher means more drag
+	UPROPERTY(EditAnywhere, Category = "Physics")
+	float DragCoefficient;
+
+	UPROPERTY(EditAnywhere, Category = "Physics")
+	class UBoxComponent* Collider;
 
 #pragma endregion	
 
@@ -45,11 +78,27 @@ private:
 
 	void MoveForward(float Value);
 
+	void MoveRight(float Value);
+
 	FVector Velocity;
 
 	float Throttle;
 
+	float SteeringThrow;
+
 
 #pragma endregion	
+
+#pragma region Physics Functions
+
+	FVector GetResistance();
+
+#pragma endregion
+
+#pragma region Movement Functions
+
+	void UpdateLocationFromVelocity(float DeltaTime);
+
+#pragma endregion
 
 };
